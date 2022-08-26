db8014a838923466d458648608a28767631443c0
atetheone <atevirran@gmail.com>
2022-08-26 12:18:46 +0100

    update howSum

--- dynamic_programing/howSum.js
+++ dynamic_programing/howSum.js
@@ -4,25 +4,29 @@
  * @param {number[]} nums 
  * @returns {number[]}
  */
- const howSum = (target, nums, li=[]) => {
-    console.log('the list ' + li)
+ const howSum = (target, nums, mem={}) => {
+
+    if (target in mem) return mem[target]
     if (target < 0) return null;
-    if (target === 0) return li;
+    if (target === 0) return [];
 
     for (let el of nums) {
         let diff = target - el;
         
-        let r = howSum(diff, nums, [...li, el]);
-        if (r != null) return r;
+        let r = howSum(diff, nums, mem);
+        if (r != null) {
+            mem[target] =  [...r, el]
+            return mem[target];
+        }
 
     }
+    mem[target] = null
+    return mem[target];
 
-    return null;
-
-}
+} 
 
 /**
  * Input: target = 7, nums = [5, 3, 4, 7]
  */
-let t = 7, nums = [5, 3, 4, 7];
+let t = 7, nums = [1, 2];
 console.log(howSum(t, nums))
